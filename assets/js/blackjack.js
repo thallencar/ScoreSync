b0acc3990a3b2696c0d072534f203f2958c0d79a
thallencar <alencar.thalita@gmail.com>
2023-12-14 21:26:16 -0300

    atualizando js das tabelas na page blackjack

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -1,6 +1,7 @@
+//TABELA 1 - Pontuação Geral
 //Cada player terá uma pontuação diferente
-const players = document.querySelectorAll('.player');
-players.forEach(player => {
+const players1 = document.querySelectorAll('.table-bj-pts-gerais .player');
+players1.forEach(player => {
   player.vitoria = 0;
   player.empate = 0;
   player.derrota = 0;
@@ -14,7 +15,6 @@ players.forEach(player => {
   const empateCell = vitoriaCell.nextElementSibling;
   const derrotaCell = empateCell.nextElementSibling;
 
-
   empateCell.addEventListener('click', () => {
     atualizaPlacar(player, 'empate');
   });
@@ -23,7 +23,6 @@ players.forEach(player => {
     atualizaPlacar(player, 'derrota');
   });
 });
-
 
 function atualizaPlacar(player, type) {
   //Atualiza a pontuação do jogador
@@ -41,14 +40,59 @@ function atualizaPlacar(player, type) {
   atualizaPlacarTotal(player);
 }
 
+//Função para resetar o placar
 function atualizaPlacarTotal(player) {
   const scoreCell = player.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
   scoreCell.textContent = player.vitoria * 3 + player.empate;
 }
 
-//Função para resetar o placar
+//TABELA 2 - Rodadas
+const players2 = document.querySelectorAll('.table-bj-rodadas .player');
+players2.forEach(player => {
+  player.jack = 0;
+  player.apostaMaiorQueDealer = 0;
+  player.apostaMenorQueDealer = 0;
+  player.apostaEstourada = 0;
+
+  player.addEventListener('click', () => {
+    atualizaPlacar2(player, 'jack');
+  });
+
+  const jackCell = player.nextElementSibling;
+  const apostaMaiorQueDealerCell = jackCell.nextElementSibling;
+  const apostaMenorQueDealerCell = apostaMaiorQueDealerCell.nextElementSibling;
+  const apostaEstouradaCell = apostaMenorQueDealerCell.nextElementSibling;
+
+  apostaMaiorQueDealerCell.addEventListener('click', () => {
+    atualizaPlacar2(player, 'apostaMaiorQueDealer');
+  });
+
+  apostaMenorQueDealerCell.addEventListener('click', () => {
+    atualizaPlacar2(player, 'apostaMenorQueDealer');
+  });
+
+  apostaEstouradaCell.addEventListener('click', () => {
+    atualizaPlacar2(player, 'apostaEstourada');
+  });
+});
+
+function atualizaPlacar2(player, type) {
+  player[type]++;
+
+  const jackCell = player.nextElementSibling;
+  const apostaMaiorQueDealerCell = jackCell.nextElementSibling;
+  const apostaMenorQueDealerCell = apostaMaiorQueDealerCell.nextElementSibling;
+  const apostaEstouradaCell = apostaMenorQueDealerCell.nextElementSibling;
+
+  jackCell.textContent = player.jack;
+  apostaMaiorQueDealerCell.textContent = player.apostaMaiorQueDealer;
+  apostaMenorQueDealerCell.textContent = player.apostaMenorQueDealer;
+  apostaEstouradaCell.textContent = player.apostaEstourada;
+}
+
 function reiniciarPlacar() {
-  players.forEach(player => {
+  // Reiniciar TABELA 1
+  players1.forEach(player => {
     player.vitoria = 0;
     player.empate = 0;
     player.derrota = 0;
@@ -63,5 +107,22 @@ function reiniciarPlacar() {
 
     atualizaPlacarTotal(player);
   });
-}
 
+  // Reiniciar TABELA 2
+  players2.forEach(player => {
+    player.jack = 0;
+    player.apostaMaiorQueDealer = 0;
+    player.apostaMenorQueDealer = 0;
+    player.apostaEstourada = 0;
+
+    const jackCell = player.nextElementSibling;
+    const apostaMaiorQueDealerCell = jackCell.nextElementSibling;
+    const apostaMenorQueDealerCell = apostaMaiorQueDealerCell.nextElementSibling;
+    const apostaEstouradaCell = apostaMenorQueDealerCell.nextElementSibling;
+
+    jackCell.textContent = 0;
+    apostaMaiorQueDealerCell.textContent = 0;
+    apostaMenorQueDealerCell.textContent = 0;
+    apostaEstouradaCell.textContent = 0;
+  });
+}
